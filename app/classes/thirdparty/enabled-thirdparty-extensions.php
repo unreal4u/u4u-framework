b6def45d6949a2c5406aacb570d7d0522876c748
Camilo Sperberg <me@unreal4u.com>
2013-05-22 23:57:56 +0200

    Added Smarty to thirdparty extensions

--- app/classes/thirdparty/enabled-thirdparty-extensions.php
+++ app/classes/thirdparty/enabled-thirdparty-extensions.php
@@ -12,3 +12,4 @@ define('TP_UPLOAD',     'class.upload_v0.31/class.upload.php');
 define('TP_PHPGETTEXT', 'php-gettext-1.0.11/gettext.inc');
 define('TP_PHPMAILER',  'PHPMailer_5.2.1/class.phpmailer.php');
 define('U4U_CLASSES',   'u4u-classes/stable-versions.php');
+define('TP_SMARTY',     'Smarty-3.1.13/libs/Smarty.class.php');
